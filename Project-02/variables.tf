df77842f7257507bc06d37bf875ae7e82a9dc3e1
faizakashaf <faizakashaf4@gmail.com>
2025-01-17 22:18:05 +0500

    addedd deleted files

---
+++ Project-02/variables.tf
@@ -0,0 +1,11 @@
+variable "nba_api_key" {
+  type = string
+  description = "this is an npa api key to interact with sports data api"
+  sensitive = true
+}
+
+
+variable "email_subscriber" {
+  type = string
+  description = "this will be subscriber email."
+}
